ee049fe84cab93e65f777d1b67806fc42b150a89
Horizonte Junior <hnmagalhaesjr@gmail.com>
2025-03-06 15:59:03 -0300

    Ajustando fotos 5

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,24 +1,6 @@
 import { Stations } from "@/components/stations";
 import { getStations } from "@/services/get-stations";
 
-// import Link from "next/link";
-
-// export default function MobileFeatures() {
-//   return (
-//     <div className="flex flex-col items-center space-y-4 p-4">
-//       <Link href="/checkin">
-//         <button className="px-4 py-2 bg-primary text-white rounded">
-//           Check-In
-//         </button>
-//       </Link>
-//       <Link href="/checkout">
-//         <button className="px-4 py-2 bg-blue-500 text-white rounded">
-//           Check-Out
-//         </button>
-//       </Link>
-//     </div>
-//   );
-// }
 
 export default async function Dashboard() {
   const stations = await getStations();
